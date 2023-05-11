9dc80256fb763f61ea39d533a0c8e85e231fcd7b
Jeff Lin <chienpu.lin@student.unimelb.edu.au>
2023-05-11 13:44:11 +1000

    update connector

--- database/couchdb_connector.py
+++ database/couchdb_connector.py
@@ -9,6 +9,17 @@ DATABASE = "twitter"
 class Connector:
 
     def __init__(self, username=USERNAME, password=PASSWORD, host=HOST, port=PORT, database=DATABASE):
+        """
+        Connector initialization
+
+        Parameters:
+            username: couchdb username; default = "admin"
+            password: couchdb password; default = "admin"
+            host:     couchdb host ip address; default = "172.26.136.58"
+            port:     couchdb port; default = "5984"
+            database: couchdb database to connect; will create a new database if cannot find it;
+                      default = "twitter"
+        """
         self.username = username
         self.password = password
         self.host = host
@@ -22,4 +33,10 @@ class Connector:
             self.database = self.server.create(database)
 
     def put(self, doc):
+        """
+        Put document onto the couchdb datebase
+
+        Parameters:
+            doc: the doc to put
+        """
         self.database.save(doc)
